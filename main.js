8ce54921dd5ada308429b9d6a9a6f273c2aa19a6
simonnguyenlife <simonnguyenlife@gmail.com>
2024-06-15 11:26:01 +0700

    push

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { CLOSE_ICON, MESSAGE_ICON, styles } from "./assets.js";
+
 class MessageWidget {
   constructor(position = "bottom-right") {
     this.position = this.getPosition(position);
@@ -20,9 +21,6 @@ class MessageWidget {
   }
 
   async initialize() {
-    /**
-     * Create and append a div element to the document body
-     */
     const container = document.createElement("div");
     container.style.position = "fixed";
     Object.keys(this.position).forEach(
@@ -30,49 +28,28 @@ class MessageWidget {
     );
     document.body.appendChild(container);
 
-    /**
-     * Create a button element and give it a class of button__container
-     */
     const buttonContainer = document.createElement("button");
     buttonContainer.classList.add("button__container");
 
-    /**
-     * Create a span element for the widget icon, give it a class of 'widget__icon', update it's innerHTML property to an icon which would serve as the widget icon.
-     */
     const widgetIconElement = document.createElement("span");
     widgetIconElement.innerHTML = MESSAGE_ICON;
     widgetIconElement.classList.add("widget__icon");
     this.widgetIcon = widgetIconElement;
 
-    /**
-     * Create a span element for the close icon, give it a class of 'widget__icon' and 'widget__hidden' which would be removed whenever the widget is closed, update it's innerHTML property to an icon which would serve as the widget icon during that state.
-     */
     const closeIconElement = document.createElement("span");
     closeIconElement.innerHTML = CLOSE_ICON;
     closeIconElement.classList.add("widget__icon", "widget__hidden");
     this.closeIcon = closeIconElement;
 
-    /**
-     * Append both icons created to the button element and add a `click` event listener on the button to toggle the widget open and close.
-     */
     buttonContainer.appendChild(this.widgetIcon);
     buttonContainer.appendChild(this.closeIcon);
     buttonContainer.addEventListener("click", this.toggleOpen.bind(this));
 
-    /**
-     * Create a container for the widget and add the following classes:- "widget__hidden", "widget__container"
-     */
     this.widgetContainer = document.createElement("div");
     this.widgetContainer.classList.add("widget__hidden", "widget__container");
 
-    /**
-     * Invoke the `createWidget()` method
-     */
     this.createWidgetContent();
 
-    /**
-     * Append the widget's content and the button to the container
-     */
     container.appendChild(this.widgetContainer);
     container.appendChild(buttonContainer);
   }
@@ -154,17 +131,21 @@ class MessageWidget {
 
 export function initializeWidget(props) {
   console.log(props);
-  return new MessageWidget();
+  return new MessageWidget(props.position);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const scriptTags = document.getElementsByTagName("script");
-  const currentScript = Array.from(scriptTags).find((script) =>
-    script.src.includes("main.min.js")
-  );
-  if (currentScript) {
-    const position =
-      currentScript.getAttribute("data-position") || "bottom-right";
+  // Attempt to get the current script tag using different methods
+  const scriptTag =
+    document.currentScript ||
+    Array.from(document.getElementsByTagName("script")).find((script) =>
+      script.src.includes("main.min.js")
+    );
+
+  if (scriptTag) {
+    const position = scriptTag.getAttribute("data-position") || "bottom-right";
     initializeWidget({ position });
+  } else {
+    console.error("Script tag not found.");
   }
 });
