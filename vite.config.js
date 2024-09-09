ab99b2dbabf6748984b90f861207ac2f0337b2f2
simonnguyenlife <simonnguyenlife@jitera.com>
2024-09-09 14:30:18 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import { obfuscator } from "rollup-obfuscator";
 
 export default defineConfig({
   build: {
@@ -8,17 +7,6 @@ export default defineConfig({
       output: {
         entryFileNames: "widget_club_wonder.min.js",
       },
-      plugins: [
-        obfuscator({
-          transformObjectKeys: true,
-          unicodeEscapeSequence: true,
-          numbersToExpressions: true,
-          shuffleStringArray: true,
-          splitStrings: true,
-          stringArrayThreshold: 1,
-          identifierNamesGenerator: "hexadecimal",
-        }),
-      ],
     },
   },
 });
